993f90a6329ea5270dcbacd3f1d704f071f15f2f
joce162 <jo.carvacho@duocuc.cl>
2026-06-17 23:56:36 -0400

    Delete notificacion directory

--- notificacion/src/main/java/com/mayorista/notificacion/repository/notificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mayorista.notificacion.repository;
-
-import com.mayorista.notificacion.model.notificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface notificacionRepository extends JpaRepository<notificacion, Long> {
-}
